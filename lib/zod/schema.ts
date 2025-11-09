c867993dfa52e50fe72e5a379c08acd2fac135ab
DannyLurker <dannycode992@gmail.com>
2025-11-09 19:17:12 +0700

    feat: update schema validation for password and add confirmPassword; create initial TodoList

--- lib/zod/schema.ts
+++ lib/zod/schema.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 const schema = z.object({
-  name: z.string().min(3),
   username: z.string().min(3),
   email: z.string().email(),
-  password: z.string().min(1),
+  password: z.string().min(8),
+  confirmPassword: z.string().min(8),
 });
 
 type Schema = z.infer<typeof schema>;
