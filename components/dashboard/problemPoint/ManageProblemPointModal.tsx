2176e264203bc8fff9f108b1816ae8c6ca10f36c
DannyLurker <dannycode992@gmail.com>
2026-01-11 13:01:57 +0700

    Refactored: A little fix on import

--- components/dashboard/problemPoint/ManageProblemPointModal.tsx
+++ components/dashboard/problemPoint/ManageProblemPointModal.tsx
@@ -1,39 +1,39 @@
 "use client";
 
 import {
-    Dialog,
-    DialogContent,
-    DialogHeader,
-    DialogTitle,
-    DialogDescription,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
 } from "@/components/ui/dialog";
 import ProblemPointManagement from "./ProblemPointManagement";
 import { Session } from "@/lib/types/session";
 
 interface ManageProblemPointModalProps {
-    open: boolean;
-    onOpenChange: (open: boolean) => void;
-    session: Session;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  session: Session;
 }
 
 export default function ManageProblemPointModal({
-    open,
-    onOpenChange,
-    session,
+  open,
+  onOpenChange,
+  session,
 }: ManageProblemPointModalProps) {
-    return (
-        <Dialog open={open} onOpenChange={onOpenChange}>
-            <DialogContent className="max-w-6xl max-h-[90vh] overflow-y-auto">
-                <DialogHeader>
-                    <DialogTitle>Manage Problem Points</DialogTitle>
-                    <DialogDescription>
-                        View, update, or delete existing problem point records.
-                    </DialogDescription>
-                </DialogHeader>
-                <div className="mt-4">
-                    <ProblemPointManagement session={session} />
-                </div>
-            </DialogContent>
-        </Dialog>
-    );
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent className="max-w-6xl max-h-[90vh] overflow-y-auto">
+        <DialogHeader>
+          <DialogTitle>Manage Problem Points</DialogTitle>
+          <DialogDescription>
+            View, update, or delete existing problem point records.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="mt-4">
+          <ProblemPointManagement session={session} />
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
 }
