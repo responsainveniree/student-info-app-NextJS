c2ec3b5f4b934fd04481090038d9decb3df94e49
DannyLurker <dannycode992@gmail.com>
2026-04-17 10:23:40 +0700

    chore: add metadata at root page.tsx and google verification for goolge console

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
 import LandingPage from "@/components/landing-page/LandingPage";
 import { auth } from "@/lib/auth/authNode";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "SMK Advent Student Info App",
+  description: "SMK Advent",
+
+  verification: {
+    google: "googlec2021259fa231b99",
+  },
+
+  openGraph: {
+    title: "SMK Advent Student Info App",
+    description: "SMK Advent",
+    url: "https://smk-advent.vercel.app",
+    siteName: "SMK Advent",
+    type: "website",
+  },
+};
 
 const page = async () => {
   const session = await auth();
